7f45ad795eecda2733c63e39c2530a3abc207819
David Stainton <dstainton415@gmail.com>
2018-08-26 18:24:14 +0100

    panda: Add cli args

--- panda/main.go
+++ panda/main.go
@@ -35,9 +35,13 @@ func main() {
 	var configFile string
 	var sharedSecret string
 	var message string
+	var provider string
+	var recipient string
 	genOnly := flag.Bool("g", false, "Generate the keys and exit immediately.")
 	flag.StringVar(&configFile, "c", "", "configuration file")
 	flag.StringVar(&sharedSecret, "secret", "", "the share secret")
+	flag.StringVar(&provider, "provider", "", "the Provider of the PANDA server")
+	flag.StringVar(&recipient, "recipient", "", "the recipient of the PANDA service")
 	flag.StringVar(&message, "message", "", "the secret message")
 	flag.Parse()
 
@@ -63,19 +67,15 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = c.NewSession()
+	session, err := c.NewSession()
 	if err != nil {
 		panic(err)
 	}
-	c.WaitForPKIDocument()
-
-	// XXX make this CLI options?
-	pandaServerRecipient := "+panda"
-	pandaProvider := "provider1"
+	session.WaitForPKIDocument()
 
 	// do the PANDA protocol here
 	blobSize := 10000
-	panda := pclient.New(blobSize, c, c.GetLogger("katzenpost/PANDA"), pandaServerRecipient, pandaProvider)
+	panda := pclient.New(blobSize, session, c.GetLogger("katzenpost/PANDA"), recipient, provider)
 	kx, err := crypto.NewKeyExchange(rand.Reader, panda, []byte(sharedSecret), []byte(message))
 	if err != nil {
 		panic(err)
@@ -85,4 +85,7 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("GOT REPLY: %s\n", string(reply))
+
+	// shutdown the client
+	c.Shutdown()
 }
